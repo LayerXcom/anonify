9af5c0903fc78c3583d6014c5095f54e41680112
jkcomment <jkcomment@gmail.com>
2021-07-19 16:14:18 +0900

    fix EI_SIZE_MAX=4096

--- modules/key-vault-host/src/controller.rs
+++ modules/key-vault-host/src/controller.rs
@@ -1,7 +1,7 @@
 use frame_host::ecall_controller::*;
 use key_vault_ecall_types::*;
 
-pub const EI_MAX_SIZE: usize = 2000;// 4096; // 2048;
+pub const EI_MAX_SIZE: usize = 4096; // 2048;
 
 pub struct StartServerController;
 
